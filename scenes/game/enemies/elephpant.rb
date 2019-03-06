9179b166fd609d19fe03f1da4ee3cb8bab6d4375
ia17011 <cfoyvlird@gmail.com>
2019-03-06 13:33:33 +0900

    removed comments

--- scenes/game/enemies/elephpant.rb
+++ scenes/game/enemies/elephpant.rb
@@ -2,6 +2,5 @@ require_relative '../enemy.rb'
 class Elephpant < Enemy
     def initialize(x, y, r, mass, image = nil, e = 0.8, u = 0.8)
         super
-        #@img = Image.load('images/elephpant.png')
     end
 end
