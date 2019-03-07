7f9866a25aec158802b238c435aa2f91569cf467
RinTakashima <rtakashima0802@gmail.com>
2019-03-07 16:15:55 +0900

    浮遊するruby実装

--- scenes/game/ruby.rb
+++ scenes/game/ruby.rb
@@ -1,15 +1,16 @@
 
-class Ruby < CPBox
+class Ruby < CPCircle
    COLLISION_TYPE = 2
    #イメージをrubyへ
-   def initialize(x, y, width, height, mass=1, color=C_BLUE)
+   def initialize(x, y, r=15, mass=1, color=C_BLUE, e=0.8, u=0.01)
       super
       @image=Image.load("images/ruby.png")
-      @v=CP::Vec2.new(x,y)
+      #@v=CP::Vec2.new(x,y)
    end
    def move
       #上下に浮遊
-      v=@v-@body.p
-      apply_force(v.x,v.y)
+      #v=@v-@body.p
+      @body.v += CP::Vec2.new(0, -16.6)
+      #apply_force(,)
    end
 end
