02e2e653f1f927a4a29a19955e545a146d4d57c1
Masahiko Kyoriki <it1773502@gmail.com>
2019-03-07 23:54:34 +0900

    ビール、はじめました。

--- scenes/game/player.rb
+++ scenes/game/player.rb
@@ -19,26 +19,36 @@ class Player < CPCircle
 
   end
 
-def move
-   if @jumpable
-     if @body.v.x > 0
-       @current_image = @images[:right]
-     elsif @body.v.x < 0
-       @current_image = @images[:left]
-     end
-    @current_image = @images[:right] if Input.key_down?(K_RIGHT)
-    @current_image = @images[:left] if Input.key_down?(K_LEFT)
-  else
-    @current_image = @images[:jump]
-  end
-    apply_force(50, 0) if Input.key_down?(K_RIGHT)
-    apply_force(-50, 0) if Input.key_down?(K_LEFT)
-    apply_force(0, 100) if Input.key_down?(K_DOWN)
-    if Input.key_push?(K_UP) && @jumpable
-      apply_force(0, -4500)
-
+  def move
+     if @jumpable
+      if @body.v.x > 0
+        @current_image = @images[:right]
+      elsif @body.v.x < 0
+        @current_image = @images[:left]
+      end
+      @current_image = @images[:right] if Input.key_down?(K_RIGHT)
+      @current_image = @images[:left] if Input.key_down?(K_LEFT)
+    else
       @current_image = @images[:jump]
-      @jumpable = false
+    end
+    if @haveItem == [["beer", true, 0, 1]]
+      apply_force(50, 0) if Input.key_down?(K_LEFT)
+      apply_force(-50, 0) if Input.key_down?(K_RIGHT)
+      apply_force(0, 100) if Input.key_down?(K_UP)
+      if Input.key_push?(K_DOWN) && @jumpable
+        apply_force(0, -4500)
+        @current_image = @images[:jump]
+        @jumpable = false
+      end
+    else
+      apply_force(50, 0) if Input.key_down?(K_RIGHT)
+      apply_force(-50, 0) if Input.key_down?(K_LEFT)
+      apply_force(0, 100) if Input.key_down?(K_DOWN)
+      if Input.key_push?(K_UP) && @jumpable
+        apply_force(0, -4500)
+        @current_image = @images[:jump]
+        @jumpable = false
+      end
     end
   end
 
